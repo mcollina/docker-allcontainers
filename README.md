2fbd62df98e84ce3ea6f4dd35a056a3b33e2fa8a
Matteo Collina <hello@matteocollina.com>
2015-02-04 15:33:09 +0100

    Initial import.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # docker-allcontainers
+
 Get notified when a new container is started or stopped
+
+## Install
+
+```bash
+npm install docker-loghose --save
+```
+
+## Usage
+
+```js
+var allContainer = require('docker-allcontainers')
+var ee = allContainers({
+  preheat: true, // emit starts event for all already running containers
+  docker: null, // options to Dockerode
+})
+ee.on('start', function(meta, container) {
+  // container is a Dockerode Container
+  // see Dockerode API
+  console.log('started', meta)
+})
+ee.on('stop', function(meta, container) {
+  // container is a Dockerode Container
+  // see Dockerode API
+  console.log('stopped', meta)
+})
+
+// stop after 5 secs
+setTimeout(function() {
+  ee.destroy()
+}, 5000)
+```
+
+## License
+
+MIT
---
+++ allcontainers.js
@@ -0,0 +1,77 @@
+#! /usr/bin/env node
+
+var nes = require('never-ending-stream')
+var Docker = require('dockerode')
+var through = require('through2')
+var EE = require('events').EventEmitter
+
+function allContainers (opts) {
+  opts = opts || {}
+
+  var docker = new Docker(opts.docker)
+  var result = new EE()
+  var events = nes(function(cb) {
+        docker.getEvents(cb)
+      })
+
+  result.destroy = function() {
+    events.destroy()
+  }
+
+  events.pipe(through(function(chunk, enc, cb) {
+    var data = JSON.parse(chunk)
+    var container = docker.getContainer(data.id)
+    var toEmit = {
+      id: data.id,
+      image: data.from
+    }
+
+    // normalize the data format
+    data.Id = data.id
+    data.Image = data.from
+
+    switch (data.status) {
+      case 'start':
+        result.emit('start', toEmit, container)
+        break
+      case 'stop':
+      case 'die':
+        result.emit('stop', toEmit, container)
+        break
+      default:
+        // do nothing, really
+    }
+
+    cb()
+  }))
+
+  // preheat is the default
+  if (opts.preheat !== false) {
+    docker.listContainers(function(err, containers) {
+      if (err) {
+        return result.emit('error', err)
+      }
+
+      containers.forEach(function(container) {
+        result.emit('start', {
+          id: container.Id,
+          image: container.Image
+        }, docker.getContainer(container.id))
+      })
+    })
+  }
+
+  return result
+}
+
+if (require.main === module) {
+  (function() {
+    var ee = allContainers()
+    ee.on('start', function(container) {
+      console.log('started', container)
+    })
+    ee.on('stop', function(container) {
+      console.log('stopped', container)
+    })
+  })()
+}
